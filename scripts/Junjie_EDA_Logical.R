1be29802a605ffea7e929aa429271f74687b7759
Junjie <zjunjie@umich.edu>
2025-02-11 23:50:21 -0500

    use only 1000 rows of data

--- scripts/Junjie_EDA_Logical.R
+++ scripts/Junjie_EDA_Logical.R
@@ -1,5 +1,6 @@
-data <- read.csv("../../tea/bernado/TEA_2019.csv")
+library(readr)
 library(dplyr)
+data <- read_csv("../../tea/bernado/TEA_2019.csv", n_max = 1000)
 
 ## Data
 ##We check data for each predictor variable. 
