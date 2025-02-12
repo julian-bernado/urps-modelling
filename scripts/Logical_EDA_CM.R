75d8889714c5969a2aa454b272a989283430b311
Caroline Moy <cmoy10@icloud.com>
2025-02-11 21:37:30 -0500

    Num NA

--- scripts/Logical_EDA_CM.R
+++ scripts/Logical_EDA_CM.R
@@ -6,11 +6,16 @@ library(dplyr)
 
 # add nrow argument 
 # read_csv readr (tidy) 
-df <- read.csv("../../tea/bernado/TEA_2019.csv")
+df <- read_csv("../../tea/bernado/TEA_2019.csv", nmax = 100)
 
 
 #save png file
 # comit and push that 
 
 
-colSums(is.na(df))
+#colSums(is.na(df))
+
+data.frame(colSums(is.na(df))) %>% 
+  mutate(total_na = colSums.df.) %>% 
+  select(total_na) %>% 
+  arrange(desc(total_na))
