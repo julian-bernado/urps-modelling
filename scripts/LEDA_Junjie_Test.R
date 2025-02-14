84c476d7e5899cb97869d378c0613c2009f90fc5
Junjie <zjunjie@umich.edu>
2025-02-14 03:01:10 -0500

    get class

--- scripts/LEDA_Junjie_Test.R
+++ scripts/LEDA_Junjie_Test.R
@@ -2,4 +2,9 @@ library(readr)
 library(dplyr)
 data <- read_csv("../../tea/bernado/TEA_2019.csv")
 
-nrow(data)
+#nrow(data)
+
+# Have a look at the `class` of the variables
+df_class <- data.frame(classes = sapply(data, class))
+
+print(df_class)
