63448bc9294eaa5fe7b2822c11662ba63ecb716b
Junjie <zjunjie@umich.edu>
2025-02-14 03:20:22 -0500

    test: num of unique values

--- scripts/LEDA_Junjie_Test.R
+++ scripts/LEDA_Junjie_Test.R
@@ -5,6 +5,16 @@ data <- read_csv("../../tea/bernado/TEA_2019.csv")
 #nrow(data)
 
 # Have a look at the `class` of the variables
-df_class <- data.frame(classes = sapply(data, class))
+df1 <- data.frame(classes = sapply(data, class))
 
-print(df_class)
+print(df1)
+
+df1 <- df1 |> 
+  mutate(num_uni_value = sapply(data, function(x) 
+    {
+    count <- length(unique(x))
+    if (count > 1000) -1 else count
+    })
+  )
+
+print(df1)
