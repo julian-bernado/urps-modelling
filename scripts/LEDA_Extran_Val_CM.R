7b2363216a62e3481e1bc49470b14b576fdffc14
Caroline Moy <cmoy10@icloud.com>
2025-02-14 09:59:51 -0500

    Some edits

--- scripts/LEDA_Extran_Val_CM.R
+++ scripts/LEDA_Extran_Val_CM.R
@@ -8,4 +8,8 @@ library(tidyverse)
 # read_csv readr (tidy) 
 df <- read_csv("../../tea/bernado/TEA_2019.csv", n_max = 100)
 
-colnames(df)
+# 5-number summary 
+df %>% 
+  select_if(is.numeric) %>% 
+  sapply(MARGIN = 2, summary) 
+
--- scripts/LEDA_NA_CM.R
+++ scripts/LEDA_NA_CM.R
@@ -1,21 +1,12 @@
 # Logical EDA 
 
-# Last Updated Tuesday February 11th, 2025 10:13PM
-
 library(dplyr)
 library(readr)
 library(tidyverse)
 
-# add nrow argument 
-# read_csv readr (tidy) 
+# temporary n_max so it runs faster, will change for final run  
 df <- read_csv("../../tea/bernado/TEA_2019.csv", n_max = 100)
 
-
-#save png file
-# comit and push that 
-
-
-#colSums(is.na(df))
 
 na_df = data.frame(colSums(is.na(df))) %>% 
   mutate(total_na = colSums.is.na.df..) %>% 
@@ -28,9 +19,17 @@ na_df %>%
   dplyr::filter(total_na == 0)
 
 na_df %>% 
+  dplyr::filter(total_na > nrow(df) * 0.75)
+
+na_df %>% 
+  dplyr::filter(total_na > nrow(df) * 0.90)
+
+na_df %>% 
+  dplyr::filter(total_na == nrow(df))
+
+na_df %>% 
   ggplot() +
   geom_histogram(aes(x = total_na))
 ggsave("na.pdf")
 
-# Need to review regex
-#na_df %>% group_by('(.*)(?=_$)')
+# Next is to group by variables that have the base variable (before the _m0 or _p1)
